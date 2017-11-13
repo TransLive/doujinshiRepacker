cb985185f9d23e1462e672622008a98199692607
Translive <translivie@gmail.com>
2017-11-14 03:21:29 +0900

    update

--- zipAll.sh
+++ zipAll.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 path="$1"
-if [ -d "$path/ex" ];then
+if [ -z $path ];then
+  #echo "確定對當前目錄" $(pwd) "執行？"
+  read -p "確定對當前目錄 $(pwd) 執行(y/any)？" permit
+  if [[ $permit != y ]];then
+    exit
+  else
+    path=$(pwd)
+  fi
+fi
+
+if [ -d "$path" ];then
   cd "$path"
 else
   echo "not a path"
   exit
 fi
+
 rename 's/ /_/g' *
 zipDirs=$(ls -F | grep "/$")
 for dir in $zipDirs
